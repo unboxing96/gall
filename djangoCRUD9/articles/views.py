ec6c7eae47b2838cc32df49d5baee64daf03dc9c
unboxing96 <unboxing96@gmail.com>
2022-10-13 12:17:52 +0900

    CRUD9

---
+++ djangoCRUD9/articles/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from .forms import ArticleForm
+from .models import Article
+
+# Create your views here.
+def index(request):
+    articles = Article.objects.all().order_by("-pk")
+    context = {
+        "articles": articles,
+    }
+    return render(request, "articles/index.html", context)
+
+
+def create(request):
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("articles:index")
+    else:
+        form = ArticleForm()
+
+    context = {
+        "form": form,
+    }
+    return render(request, "articles/create.html", context)
+
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        "article": article,
+    }
+    return render(request, "articles/detail.html", context)
