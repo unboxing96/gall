664b6b54cc4053f93edac682d226635302393767
unboxing96 <unboxing96@gmail.com>
2022-10-18 17:56:58 +0900

    CRUD 13

---
+++ djangoCRUD13/articles/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    username = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
