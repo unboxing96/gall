ff33080b16be2bfd91d628ea94fdc03f90a6f654
unboxing96 <unboxing96@gmail.com>
2022-10-21 18:51:18 +0900

    #6 검색, 페이지네이션

--- djangoCRUD14/articles/urls.py
+++ djangoCRUD14/articles/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
         views.comment_delete,
         name="comment_delete",
     ),
+    path("search/", views.search, name="search"),
 ]
--- djangoCRUD14/articles/views.py
+++ djangoCRUD14/articles/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render, redirect
 from .models import Article, Comment
 from .forms import ArticleForm, CommentForm
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 def index(request):
     articles = Article.objects.all().order_by("-id")
+    page = request.GET.get("page")  # 현재 몇 번째 페이지인지
+    paginator = Paginator(articles, 10)  # (글 목록, 1페이지에 몇 개씩?)
+    page_obj = paginator.get_page(page)  # 몇 번째 페이지 가져올지
+    page_range = paginator.page_range
+
     context = {
         "articles": articles,
+        "page_obj": page_obj,
+        "page_range": page_range,
     }
     return render(request, "articles/index.html", context)
 
@@ -81,3 +90,31 @@ def comment_delete(request, article_pk, comment_pk):
     if request.user == comment.user:
         comment.delete()
     return redirect("articles:detail", article_pk)
+
+
+def search(request):
+    search_1 = request.GET.get("search_1")
+    search_2 = request.GET.get("search_2")
+
+    if search_1 == "title":
+        search_articles = Article.objects.filter(title__icontains=search_2)
+    elif search_1 == "content":
+        search_articles = Article.objects.filter(content__icontains=search_2)
+    elif search_1 == "username":
+        search_articles = Article.objects.filter(
+            user_id=get_user_model().objects.get(username=search_2)
+        )
+
+    page = request.GET.get("page")
+    paginator = Paginator(search_articles, 10)
+    page_obj = paginator.get_page(page)
+    page_range = paginator.page_range
+
+    context = {
+        "search_1": search_1,
+        "search_2": search_2,
+        "search_articles": search_articles,
+        "page_obj": page_obj,
+        "page_range": page_range,
+    }
+    return render(request, "articles/search.html", context)
