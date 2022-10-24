29d9cfaba2e17ebac6f74e5570f4d9bd5e64fc3b
unboxing96 <unboxing96@gmail.com>
2022-10-24 17:37:35 +0900

    feat: 게시글 좋아요 / 댓글 좋아요 추가 - 게시글: 좋아요 / 가장 최근 좋아요한 유저 네임 보이게 / 개수 - 댓글: 좋아요 / 개수

--- djangoCRUD14/articles/models.py
+++ djangoCRUD14/articles/models.py
@@ -18,3 +18,6 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_comment_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="like_comments"
+    )
