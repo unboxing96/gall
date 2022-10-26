19666aede1ae4035600060c63a8f3f85dc68bea4
unboxing96 <unboxing96@gmail.com>
2022-10-26 20:23:10 +0900

    feat: 팔로우 기능 추가 - 비동기로 구현..

--- djangoCRUD14/accounts/urls.py
+++ djangoCRUD14/accounts/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path("update/", views.update, name="update"),
     path("delete/", views.delete, name="delete"),
     path("password_change/", views.password_change, name="password_change"),
+    path("<int:user_pk>/follow/", views.follow, name="follow"),
 ]
